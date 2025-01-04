abba93f5c55459f30659a2606b958c5845664f3c
qing <letjinxgo@gmail.com>
2025-01-04 13:25:07 +0800

    update charts

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 set -eux -o pipefail
 packages=$(find . -maxdepth 1 -type d ! -name ".*" ! -name "charts"|xargs)
 helm package $packages --destination ./charts
-helm repo index ./charts --url https://awwcmon.github.io/helm-charts/charts
+helm repo index ./charts
 git add .
 git commit -m "update charts"
 git push
