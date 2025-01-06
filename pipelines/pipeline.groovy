8998a833093690f8bbe7da47c51b6d8507583036
qing <letjinxgo@gmail.com>
2025-01-06 21:14:42 +0800

    update charts

---
+++ pipelines/pipeline.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    agent {
+        kubernetes {
+            label 'sheer-tools' // 引用预定义模板的 Label
+        }
+    }
+    stages {
+        stage('Run in Predefined Pod Template') {
+            steps {
+            sh """
+            cat /root/.kube/kuberconfig.yaml
+            cat /root/.docker/config.json
+            """
+            }
+        }
+    }
+}
