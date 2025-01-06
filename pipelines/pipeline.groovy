b97e351d9df1e7d17881377adf5f301111ebb626
qing <letjinxgo@gmail.com>
2025-01-06 22:59:30 +0800

    update charts

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -5,61 +5,61 @@ pipeline {
 apiVersion: "v1"
 kind: "Pod"
 metadata:
-annotations:
+  annotations:
     kubernetes.jenkins.io/last-refresh: "1736173871371"
-labels:
+  labels:
     jenkins/jenkins-jenkins-agent: "true"
     jenkins/label-digest: "500b4f18aee87616849e4f4c2435020898e34aa0"
     jenkins/label: "jenkins-jenkins-agent"
     kubernetes.jenkins.io/controller: "http___jenkins_default_svc_cluster_local_8080x"
-name: "default-4rzsg"
-namespace: "default"
+  name: "default-4rzsg"
+  namespace: "default"
 spec:
-containers:
-- args:
-    - /usr/local/bin/jenkins-agent
+  containers:
+  - args:
+    - "/usr/local/bin/jenkins-agent"
     - "default-4rzsg"
     env:
     - name: "JENKINS_SECRET"
-    value: "********"
+      value: "********"
     - name: "JENKINS_TUNNEL"
-    value: "jenkins-agent.default.svc.cluster.local:50000"
+      value: "jenkins-agent.default.svc.cluster.local:50000"
     - name: "JENKINS_AGENT_NAME"
-    value: "default-4rzsg"
+      value: "default-4rzsg"
     - name: "REMOTING_OPTS"
-    value: "-noReconnectAfter 1d"
+      value: "-noReconnectAfter 1d"
     - name: "JENKINS_NAME"
-    value: "default-4rzsg"
+      value: "default-4rzsg"
     - name: "JENKINS_AGENT_WORKDIR"
-    value: "/home/jenkins/agent"
+      value: "/home/jenkins/agent"
     - name: "JENKINS_URL"
-    value: "http://jenkins.default.svc.cluster.local:8080/"
+      value: "http://jenkins.default.svc.cluster.local:8080/"
     image: "sheer/tools:latest"
     imagePullPolicy: "IfNotPresent"
     name: "jnlp"
     resources:
-    limits:
+      limits:
         memory: "2048Mi"
         cpu: "2048m"
-    requests:
+      requests:
         memory: "1024Mi"
         cpu: "1024m"
     securityContext:
-    privileged: true
+      privileged: true
     tty: true
     volumeMounts:
     - mountPath: "/home/jenkins/agent"
-    name: "workspace-volume"
-    readOnly: false
+      name: "workspace-volume"
+      readOnly: false
     workingDir: "/home/jenkins/agent"
-hostNetwork: false
-nodeSelector:
+  hostNetwork: false
+  nodeSelector:
     kubernetes.io/os: "linux"
-restartPolicy: "Never"
-serviceAccountName: "default"
-volumes:
-- emptyDir:
-    medium: ""
+  restartPolicy: "Never"
+  serviceAccountName: "default"
+  volumes:
+  - emptyDir:
+      medium: ""
     name: "workspace-volume"
 """
         }
