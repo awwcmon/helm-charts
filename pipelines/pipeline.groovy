fcd846500111f228d00680e81f2d41a7c6cab428
qing <letjinxgo@gmail.com>
2025-01-06 22:53:19 +0800

    update charts

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -2,30 +2,59 @@ pipeline {
     agent {
         kubernetes {
             yaml """
-apiVersion: v1
-kind: Pod
+apiVersion: "v1"
+kind: "Pod"
+metadata:
+labels:
+    jenkins/jenkins-jenkins-agent: "true"
+    jenkins/label: "jenkins-jenkins-agent"
+    kubernetes.jenkins.io/controller: "http___jenkins_default_svc_cluster_local_8080x"
+name: "default-4rzsg"
+namespace: "default"
 spec:
-  containers:
-  - name: sheer-tools
-    image: sheer/tools:latest
-    volumeMounts:
-    - name: dockerconfig
-      mountPath: /home/jenkins/.docker
-      readOnly: true
-    - name: kubeconfig
-      mountPath: /home/jenkins/.kube
-      readOnly: true
+containers:
+    env:
+    - name: "JENKINS_SECRET"
+    value: "********"
+    - name: "JENKINS_TUNNEL"
+    value: "jenkins-agent.default.svc.cluster.local:50000"
+    - name: "JENKINS_AGENT_NAME"
+    value: "default-4rzsg"
+    - name: "REMOTING_OPTS"
+    value: "-noReconnectAfter 1d"
+    - name: "JENKINS_NAME"
+    value: "default-4rzsg"
+    - name: "JENKINS_AGENT_WORKDIR"
+    value: "/home/jenkins/agent"
+    - name: "JENKINS_URL"
+    value: "http://jenkins.default.svc.cluster.local:8080/"
+    image: "sheer/tools:latest"
+    imagePullPolicy: "IfNotPresent"
+    name: "jnlp"
+    resources:
+    limits:
+        memory: "2048Mi"
+        cpu: "2048m"
+    requests:
+        memory: "512Mi"
+        cpu: "512m"
     securityContext:
-      privileged: true
-    stdin: true
+    privileged: true
     tty: true
-  volumes:
-  - name: dockerconfig
-    secret:
-      secretName: dockerconfig
-  - name: kubeconfig
-    secret:
-      secretName: kubeconfig
+    volumeMounts:
+    - mountPath: "/home/jenkins/agent"
+    name: "workspace-volume"
+    readOnly: false
+    workingDir: "/home/jenkins/agent"
+hostNetwork: false
+nodeSelector:
+    kubernetes.io/os: "linux"
+restartPolicy: "Never"
+serviceAccountName: "default"
+volumes:
+- emptyDir:
+    medium: ""
+    name: "workspace-volume"
 """
         }
     }
