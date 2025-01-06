cccaab8bd82f8a4ede9ef9e9a65180a2cefd805f
qing <letjinxgo@gmail.com>
2025-01-06 23:21:50 +0800

    update charts

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -14,23 +14,26 @@ spec:
   containers:
   - args:
     - "/usr/local/bin/jenkins-agent"
-    env:
-    - name: "JENKINS_URL"
-      value: "http://jenkins.default.svc.cluster.local:8080/"
     image: "sheer/tools:latest"
     imagePullPolicy: "IfNotPresent"
     name: "jnlp"
     resources:
       limits:
-        memory: "2048Mi"
-        cpu: "2048m"
-      requests:
         memory: "1024Mi"
         cpu: "1024m"
+      requests:
+        memory: "512Mi"
+        cpu: "512m"
     securityContext:
       privileged: true
     tty: true
     volumeMounts:
+    - mountPath: "/home/jenkins/.kube"
+      name: "kubeconfig"
+      readOnly: true
+    - mountPath: "/home/jenkins/.docker"
+      name: "dockerconfig"
+      readOnly: true
     - mountPath: "/home/jenkins/agent"
       name: "workspace-volume"
       readOnly: false
@@ -44,6 +47,12 @@ spec:
   - emptyDir:
       medium: ""
     name: "workspace-volume"
+  - name: kubeconfig
+    secret:
+      secretName: kubeconfig
+  - name: dockerconfig
+    secret:
+      secretName: dockerconfig
 """
         }
     }
