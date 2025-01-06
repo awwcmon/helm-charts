2d905a6e92ddb18cccc8259179c6c6227304a4b9
qing <letjinxgo@gmail.com>
2025-01-06 22:41:09 +0800

    update charts

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -2,44 +2,37 @@ pipeline {
     agent {
         kubernetes {
             yaml """
-apiVersion: "v1"
-kind: "Pod"
-metadata:
-  labels:
-    jenkins/jenkins-jenkins-agent: "true"
-    jenkins/label: "jenkins-jenkins-agent"
-    kubernetes.jenkins.io/controller: "http___jenkins_default_svc_cluster_local_8080x"
-  name: "sheer-tools"
-  namespace: "default"
+apiVersion: v1
+kind: Pod
 spec:
   containers:
-    image: "sheer/tools:latest"
-    imagePullPolicy: "IfNotPresent"
-    name: "jnlp"
-    resources:
-      limits:
-        memory: "2048Mi"
-        cpu: "2048m"
-      requests:
-        memory: "1024Mi"
-        cpu: "1024m"
+  - name: sheer-tools
+    image: sheer/tools:latest
+    volumeMounts:
+    - name: dockerconfig
+      mountPath: /home/jenkins/.docker
+      readOnly: true
+    - name: kubeconfig
+      mountPath: /home/jenkins/.kube
+      readOnly: true
+    #command:
+    #- sh
+    #- -c
+    #- |
+      #/usr/local/bin/jenkins-agent
+      #&& export DOCKER_CONFIG=/home/jenkins/.docker/config.json && \
+      #export KUBECONFIG=/home/jenkins/.kube/kubeconfig.json && docker login && cat
     securityContext:
       privileged: true
+    stdin: true
     tty: true
-    volumeMounts:
-    - mountPath: "/home/jenkins/agent"
-      name: "workspace-volume"
-      readOnly: false
-    workingDir: "/home/jenkins/agent"
-  hostNetwork: false
-  nodeSelector:
-    kubernetes.io/os: "linux"
-  restartPolicy: "Never"
-  serviceAccountName: "default"
   volumes:
-  - emptyDir:
-      medium: ""
-    name: "workspace-volume"
+  - name: dockerconfig
+    secret:
+      secretName: dockerconfig
+  - name: kubeconfig
+    secret:
+      secretName: kubeconfig
 """
         }
     }
