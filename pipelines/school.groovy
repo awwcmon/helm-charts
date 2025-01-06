bca79907f1ea516b8e501d59ab21e7abb430beab
qing <letjinxgo@gmail.com>
2025-01-06 17:30:39 +0800

    update charts

--- pipelines/school.groovy
+++ pipelines/school.groovy
@@ -88,6 +88,7 @@ pipeline {
                     echo ".......docker push ${DOCKER_REGISTRY}/${DOCKER_USERNAME}/${params.IMAGE_NAME}......."
                     sh """
                     set -x
+                    cat ${DOCKER_CONFIG_PATH}
                     chmod 600 \$(dirname ${DOCKER_CONFIG_PATH})
                     export DOCKER_CONFIG=\$(dirname ${DOCKER_CONFIG_PATH})
                     docker login
