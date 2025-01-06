f2123a92c99e53657a64adca2fe0c61185993e31
qing <letjinxgo@gmail.com>
2025-01-06 14:32:12 +0800

    update charts

--- pipelines/school.groovy
+++ pipelines/school.groovy
@@ -38,7 +38,7 @@ pipeline {
                         file(credentialsId: env.KUBECONFIG, variable: 'KUBECONFIG_PATH')]) {
                             sh '''
                             chmod 600 $KUBECONFIG_PATH $DOCKER_CONFIG_PATH
-                            cat DOCKER_CONFIG_PATH
+                            cat $DOCKER_CONFIG_PATH
                             export KUBECONFIG=${KUBECONFIG_PATH}
                             docker login
                             kubectl get nodes
