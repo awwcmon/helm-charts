1b56a65086c074ad5ee00df8713672e408efb482
qing <letjinxgo@gmail.com>
2025-01-11 23:19:20 +0800

    update charts

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -127,8 +127,8 @@ spec:
                         sh """
                         set -x
                         make deploy CHART_REPO_NAME=${env.CHART_REPO_NAME} CHART_URL=${env.CHART_URL} \
-                        IMAGE_NAME=${params.IMAGE_NAME} TAG=${params.TAG} RELEASE_NAME=${params.RELEASE_NAME} \
-                        NAMESPACE=${env.NAMESPACE}
+                        IMAGE_NAME=${params.IMAGE_NAME} TAG=${params.TAG} NAMESPACE=${env.NAMESPACE} \
+                        RELEASE_NAME=${params.RELEASE_NAME}
                         """
                     }
                 }
